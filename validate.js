2f1f2d09890be5a889e85cad5d1cbf96545d1fe4
vitor-siqueira <vitorsiqueria@gmail.com>
2019-11-05 08:35:10 -0300

    patterns change

--- validate.js
+++ validate.js
@@ -10,6 +10,7 @@ function validCreditCard(value) {
     for (let n = value.length - 1; n >= 0; n--) {
         let cDigit = value.charAt(n),
             digit = parseInt(cDigit, 10);
+            
         if (bEven && (digit *= 2) > 9) digit -= 9;
         numCheck += digit;
         bEven = !bEven;
@@ -24,14 +25,17 @@ function validCreditCard(value) {
 //identify card type
 function GetCardType(number) {
     let regx = [
-        { name: 'Visa', re: '^4' },
-        { name: 'American Express', re: '^(34|37)' },
-        { name: 'MasterCard', re: '^5[1-5]' },
-        { name: 'Discover', re: '^6011' }
+        { name: 'Visa', pattern: /^4/ },
+        { name: 'Hipercard', pattern: /^(384100|384140|384160|606282|637095|637568|60(?!11))/ },
+        { name: 'MasterCard', pattern: /^(5[1-5]|677189)|^(222[1-9]|2[3-6]\d{2}|27[0-1]\d|2720)/ },
+        { name: 'Discover', pattern: /^(6011|65|64[4-9]|622)/ },
+        { name: 'Elo', pattern: /^(4011(78|79)|43(1274|8935)|45(1416|7393|763(1|2))|50(4175|6699|67[0-7][0-9]|9000)|627780|63(6297|6368)|650(03([^4])|04([0-9])|05(0|1)|4(0[5-9]|3[0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8])|9([2-6][0-9]|7[0-8])|541|700|720|901)|651652|655000|655021)/},
+        { name: 'American Express', pattern: /^3[47]\d{13,14}$/}
     ]
     regx.forEach((item) => {
-        if (number.match(item.re) != null) {
+        if (number.match(item.pattern) != null) {
             console.log(item.name)
         }
     })
 }
+validCreditCard(4538504036003956) 
